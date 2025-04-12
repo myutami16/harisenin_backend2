4c241c3ddd5cf34fdc1c4f87bc9b88aceb961fd6
myutami16 <mardhiyatul.utami@gmail.com>
2025-04-12 17:28:33 +0700

    feat : initial project

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+
+// TODO: Tambahkan route di sini nanti
+
+module.exports = app;
