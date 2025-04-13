15bcc4d6dd0597f136f1336348594daf5047648b
myutami16 <mardhiyatul.utami@gmail.com>
2025-04-13 14:00:36 +0700

    Feat : Setup database and connection

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
+
+const pool = require("./model/db");
+
+pool.query("SELECT NOW()", (err, res) => {
+	if (err) {
+		console.error("❌ Gagal konek DB:", err);
+	} else {
+		console.log("✅ Terkoneksi ke DB:", res.rows[0]);
+	}
+	pool.end();
+});
